aea8707fac805077562010371875924a48c733a1
hasansuryaman <hasan.suryaman@outlook.com>
2020-06-02 09:20:23 +0700

    Tugas 10 C++

---
+++ tugas10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void selamat_datang(){
+    cout <<"Selamat Datang Di ";
+}
+    
+void perkalian(){
+    cout <<"Fungsi Perkalian\n";
+}
+
+int main(){
+    selamat_datang();
+    perkalian();
+}
+
